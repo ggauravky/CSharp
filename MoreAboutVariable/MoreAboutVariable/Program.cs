5c4ee21deab95a39fbb6f6a3dcff0447741796bd
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-01-29 01:37:18 +0530

    Add initial MoreAboutVariable C# console project
    Created a new C# console application named MoreAboutVariable targeting .NET Framework 4.7.2. Includes solution and project files, basic Program.cs with user input and data type comments, and assembly metadata.

---
+++ MoreAboutVariable/MoreAboutVariable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreAboutVariable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //this is a comment
+            //readline - returns a string
+            Console.WriteLine("Please enter your name:");
+            Console.WriteLine("Hello " + Console.ReadLine());
+
+            //data types--
+
+            //integer - 4 bytes
+            //string - 2 bytes per character
+            //boolean - 1 byte
+            //float/double - 4/8 bytes
+            //char - 2 bytes
+
+
+
+
+        }
+    }
+}
