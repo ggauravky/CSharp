25dc67429f834c5c8ffdc3422752cd05a50a291b
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-05-02 01:50:11 +0530

    Add 023 Advanced .NET Concepts project
    Add new solution and project for "023 Advanced .NET Concepts" targeting .NET Framework 4.7.2. Includes csproj, App.config, AssemblyInfo and Program.cs. Program.cs contains demos for a simulated distributed call, reflection, globalization, simple auth, and XML read/write. Project file defines build configurations and common assembly references.

---
+++ 023 Advanced .NET Concepts/023 Advanced .NET Concepts/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Xml;
+
+// ==============================
+// USER CLASS (for Auth Demo)
+// ==============================
+class User
+{
+    public string Username;
+    public string Role;
+
+    public User(string username, string role)
+    {
+        Username = username;
+        Role = role;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Advanced .NET Concepts Demo ===\n");
+
+        // =========================================
+        // 1. DISTRIBUTED APPLICATION (SIMULATION)
+        // =========================================
+        Console.WriteLine("---- Distributed Application ----");
+
+        // Simulating client calling server method
+        int result = ServerAdd(5, 10);
+        Console.WriteLine("Result from Server: " + result);
+
+
+        // =========================================
+        // 2. REFLECTION
+        // =========================================
+        Console.WriteLine("\n---- Reflection ----");
+
+        Type t = typeof(string);
+
+        Console.WriteLine("Methods of String class:");
+
+        foreach (MethodInfo m in t.GetMethods())
+        {
+            Console.WriteLine(m.Name);
+        }
+
+
+        // =========================================
+        // 3. GLOBALIZATION & LOCALIZATION
+        // =========================================
+        Console.WriteLine("\n---- Globalization ----");
+
+        CultureInfo culture = new CultureInfo("hi-IN");
+
+        Console.WriteLine("Date (India): " + DateTime.Now.ToString(culture));
+        Console.WriteLine("Currency (India): " + (1000).ToString("C", culture));
+
+
+        // =========================================
+        // 4. AUTHENTICATION & AUTHORIZATION
+        // =========================================
+        Console.WriteLine("\n---- Authentication & Authorization ----");
+
+        User user = new User("Gaurav", "Admin");
+
+        // Authentication
+        if (user.Username == "Gaurav")
+        {
+            Console.WriteLine("User Authenticated");
+
+            // Authorization
+            if (user.Role == "Admin")
+            {
+                Console.WriteLine("Access Granted (Admin)");
+            }
+            else
+            {
+                Console.WriteLine("Access Denied");
+            }
+        }
+
+
+        // =========================================
+        // 5. XML WRITE
+        // =========================================
+        Console.WriteLine("\n---- XML Write ----");
+
+        XmlDocument doc = new XmlDocument();
+
+        // Create root element
+        XmlElement root = doc.CreateElement("student");
+
+        // Create child elements
+        XmlElement name = doc.CreateElement("name");
+        name.InnerText = "Gaurav";
+
+        XmlElement age = doc.CreateElement("age");
+        age.InnerText = "20";
+
+        // Append elements
+        root.AppendChild(name);
+        root.AppendChild(age);
+
+        doc.AppendChild(root);
+
+        // Save file
+        doc.Save("student.xml");
+
+        Console.WriteLine("XML File Created");
+
+
+        // =========================================
+        // 6. XML READ
+        // =========================================
+        Console.WriteLine("\n---- XML Read ----");
+
+        XmlDocument readDoc = new XmlDocument();
+        readDoc.Load("student.xml");
+
+        XmlNode node = readDoc.SelectSingleNode("//name");
+
+        Console.WriteLine("Student Name: " + node.InnerText);
+
+
+        Console.WriteLine("\n=== Program End ===");
+    }
+
+    // Simulated Server Method
+    static int ServerAdd(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ 023 Advanced .NET Concepts/023 Advanced .NET Concepts/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("023 Advanced .NET Concepts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("023 Advanced .NET Concepts")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("61d0747b-cc6c-4845-976b-26885257740e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
